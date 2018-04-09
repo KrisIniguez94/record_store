5b1c271628f2998e90bf48f69efde4b9ce052c81
Adam Cooper <adam.cooper@galvanize.com>
2018-04-09 12:00:40 -0400

    Artist routes, artist controller

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
+const artistRoutes = require('./routes/artists');
 
 app.use(morgan('combined'));
 
 app.get('/', (request, response) => {
   response.send('Welcome to my Record Store.');
 });
+
+app.use('/artists', artistRoutes);
 
 app.listen( PORT, () => {
   console.log(`Artists and Songs: listening on port no. ${PORT}...`);
